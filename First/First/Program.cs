9b49a0c982f1baca50dc604d254b8fe5d6c8d09c
12200808 <SIDSHRESTHA9999@GMAIL.COM>
2023-08-13 20:06:03 +1000

    t1

---
+++ First/First/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+
+
+    static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("Addition Claculator");
+
+        //Ask the user to enter the first number
+
+        Console.Write("Enter the first number: ");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        //Ask the user to enter the second number
+
+        Console.Write("Enter the second number; ");
+
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int result = a   88 b;
+
+        Console.WriteLine("The total is:" + result);
+
+
+
+        Console.Read();
+
+    }
+}
